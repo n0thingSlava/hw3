ce801c26683f2d38723d18e695ddbdd423dcf290
Slav1k <maznevslava@yandex.ru>
2022-08-03 22:07:49 +0500

    дз

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,35 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        //Задание №1
+        System.out.println("Задание №1");
         int clientOS = 1;
         if (clientOS == 0) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        System.out.println("Задание №2");
+        int clientOS2 = 1;
+        int classClient = 2020;
+        if (clientOS2 == 1) {
+            if (classClient < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else {
+            if (classClient < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+        System.out.println("Задание №3");
+        int year = 2020;
+        if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является высокосным");
+        } else {
+            System.out.println(year + " год не является высокосным");
         }
     }
 }
