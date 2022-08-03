cf16856bed16fd1f329199e978aee8acf6b26eb5
Slav1k <maznevslava@yandex.ru>
2022-08-03 20:26:17 +0500

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         int clientOS = 1;
-        if (clientOS == 0) {
-            System.out.println("Установи Android");
+        int classClient = 2020;
+        if (clientOS == 1) {
+            if (classClient < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
         } else {
-            System.out.println("Установи iOS");
-        }
-
-
+            if (classClient < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
             }
         }
+    }
+}
