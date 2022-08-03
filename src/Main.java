e46794a5ccd4b8260412ac5a4bc43bdb2f3821ff
Slav1k <maznevslava@yandex.ru>
2022-08-03 19:36:11 +0500

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установи Android");
+        } else {
+            System.out.println("Установи iOS");
+        }
 
-        System.out.println("Hello world!");
-    }
-}
+
+            }
+        }
