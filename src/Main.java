c7f606c84f8162265f4678919985b0e09992bfef
Slav1k <maznevslava@yandex.ru>
2022-08-04 22:08:15 +0500

    дз

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание №1");
-        int clientOS = 1;
-        if (clientOS == 0) {
+        int clientOS = -1;
+        if (clientOS <= 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
+        }else {
+        if(clientOS >=1)
             System.out.println("Установите версию приложения для Android по ссылке");
         }
         System.out.println("Задание №2");
